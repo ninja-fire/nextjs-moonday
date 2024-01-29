261a7b868bc1788cde3e096acf4e8dabb3172e1b
Manon Brousse <pandillamanolito@gmail.com>
2024-01-29 14:46:17 +0700

    fontclash import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./ui/globals.css";
+import { fontclash } from "./ui/font"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fontclash.className}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/font.ts
@@ -0,0 +1,6 @@
+import localFont from 'next/font/local'
+
+export const fontclash = localFont({
+    src: './ClashDisplay-Variable.woff2',
+    display: 'swap',
+  })
