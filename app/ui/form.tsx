fa01ec2098a3520476d3ac883d454bae442215cf
Manon Brousse <pandillamanolito@gmail.com>
2024-02-05 12:46:59 +0700

    form init

--- app/ui/form.tsx
+++ app/ui/form.tsx
@@ -2,77 +2,81 @@ import Button from "./button"
 
 export default function ContactForm() {
     return(
-        <div className="light flex flex-col gap-6 py-16 px-4 md:px-20 lg:px-40 justify-center items-center overflow-y-b-clip rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
+        <div className="light flex flex-col gap-6 py-16 px-2 md:px-20 lg:px-40 justify-center items-center overflow-y-b-clip rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
 
         <div className="flex flex-col gap-4 justify-center items-center">
           <h2 className="text-center">What are your design needs?</h2>
           <p className="text-sm font-medium text-center text-caption">Our aim is to understand your unique vision and goals, and tailor our design services to suit your needs in the ever-evolving landscape of Web3 and blockchain technology.</p>
         </div>
 
-        <form className="flex flex-col gap-2 bg-bodybg/[64%] backdrop-blur-md shadow-xl border border-transparentbg rounded-3xl gap-6 px-8 py-6" id="dynamic-form">
+        <form className="flex flex-col gap-6 px-4 md:px-8 py-6 bg-bodybg/[64%] backdrop-blur-md shadow-xl border border-transparentbg rounded-3xl" id="dynamic-form">
           <div className="flex pb-4 border-b border-transparentbg">
             <h4>Your Wish List</h4>
           </div>
-          <div className="gap-2 flex-start" id="form-content-needs">
-            <label className="form-control gap-1-row" htmlFor="branding">
-              <input type="checkbox" id="branding" name="design-needs" value="branding" />
-              <div className="gap-1-2">
+          <div className="flex flex-col md:flex-row flex-start gap-6" id="form-content-needs">
+            <div className="flex flex-col flex-start gap-6">
+            <label className="flex gap-3 items-start" htmlFor="branding">
+              <input className="w-4 h-4 rounded-xl" type="checkbox" id="branding" name="design-needs" value="branding" />
+              <div className="flex flex-col gap-1">
                 <h5 className="">Branding and Identity Design</h5>
-                <p className="text-sm font-medium text-caption">Crafting unique visual identities that represent your brands values and
+                <p className="text-xs md:text-sm font-medium text-caption">Crafting unique visual identities that represent your brands values and
                   resonate with your target audience.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="print">
+            <label className="flex gap-3 items-start" htmlFor="print">
               <input type="checkbox" id="print" name="design-needs" value="print" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Print Design</h5>
-                <p className="text-sm font-medium text-caption">Creating compelling designs for print materials such as brochures,
+                <p className="text-xs md:text-sm font-medium text-caption">Creating compelling designs for print materials such as brochures,
                   business cards, posters, and more.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="social">
+            <label className="flex gap-3 items-start" htmlFor="social">
               <input type="checkbox" id="social" name="design-needs" value="social" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Social Media Graphics</h5>
-                <p className="text-sm font-medium text-caption">Crafting eye-catching graphics optimized for social media platforms to
+                <p className="text-xs md:text-sm font-medium text-caption">Crafting eye-catching graphics optimized for social media platforms to
                   boost your online presence.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="app">
+            <label className="flex gap-3 items-start" htmlFor="app">
               <input type="checkbox" id="app" name="design-needs" value="app" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Blockchain Application</h5>
-                <p className="text-sm font-medium text-caption">Crafting user-friendly and visually appealing interfaces for blockchain
+                <p className="text-xs md:text-sm font-medium text-caption">Crafting user-friendly and visually appealing interfaces for blockchain
                   platforms, wallets, and decentralized applications.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="website">
+            </div>
+            <div className="flex flex-col flex-start gap-6">
+            <label className="flex gap-3 items-start" htmlFor="website">
               <input type="checkbox" id="website" name="design-needs" value="website" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Web3 Website Design</h5>
-                <p className="text-sm font-medium text-caption">Designing immersive websites that seamlessly integrate blockchain
+                <p className="text-xs md:text-sm font-medium text-caption">Designing immersive websites that seamlessly integrate blockchain
                   technology, providing interactive and engaging experiences.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="metaverse">
+            <label className="flex gap-3 items-start" htmlFor="metaverse">
               <input type="checkbox" id="metaverse" name="design-needs" value="metaverse" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Brand Integration into Metaverse</h5>
-                <p className="text-sm font-medium text-caption">Crafting user-friendly and visually appealing interfaces for blockchain
+                <p className="text-xs md:text-sm font-medium text-caption">Crafting user-friendly and visually appealing interfaces for blockchain
                   platforms, wallets, and decentralized applications.</p>
               </div>
             </label>
-            <label className="form-control gap-1-row" htmlFor="consulting">
+            <label className="flex gap-3 items-start" htmlFor="consulting">
               <input type="checkbox" id="consulting" name="design-needs" value="consulting" />
-              <div className="gap-1-2">
+              <div className="flex flex-col gap-1">
                 <h5 className="">Consultation and Ideation</h5>
-                <p className="text-sm font-medium text-caption">Collaborating with you to brainstorm, conceptualize, and refine design
+                <p className="text-xs md:text-sm font-medium text-caption">Collaborating with you to brainstorm, conceptualize, and refine design
                   ideas tailored to your specific project.</p>
               </div>
             </label>
+            </div>
           </div>
           <div className="gap-2 flex-start hidden" id="form-content-contact">
-            <p className="text-sm font-medium text-caption">Please provide your name and email so we can reach out to you. It is the
+            <p className="text-xs md:text-sm font-medium text-caption">Please provide your name and email so we can reach out to you. It is the
               first step to bringing your design project to life.</p>
 
             <div className="gap-1 width-full">
