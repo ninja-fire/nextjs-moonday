451405f6368a40365aeb5d3bff801b36a8694636
Manon Brousse <pandillamanolito@gmail.com>
2024-03-16 19:36:41 +0700

    button bg

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -6,7 +6,7 @@ export default function Button ({text="", target="", href=""}){
         href={href}
         target={target}
         className="flex buttonBG transition-all justify-center items-center p-0.5 rounded-full w-full overflow-hidden shadow-whitepers active:shadow-none">
-            <div className="flex transition-all justify-center items-center px-16 py-4 bg-bodybg w-full rounded-full text-label hover:bg-bodybg/[0.96] text-sm font-semibold">
+            <div className="flex transition-all justify-center items-center px-16 py-4 bg-bodybg w-full rounded-full text-label text-sm font-semibold">
                 <span className="inline">{text}</span>
             </div>
         </Link>
