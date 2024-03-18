645ee8b8a29c90138892966371acc568ab2933e7
Manon Brousse <pandillamanolito@gmail.com>
2024-03-18 12:40:31 +0800

    fix responsiv and image services list

--- app/ui/serviceslist.tsx
+++ app/ui/serviceslist.tsx
@@ -7,10 +7,10 @@ import gradient from '../../public/gradient.webp';
 
 export default function ServicesList() {
     return (
-        <div className="flex flex-wrap lg:flex-nowrap gap-4 items-center w-full justify-center">
-            <div className="flex flex-wrap gap-4 w-full">
+        <div className="flex flex-col gap-4 items-center w-full justify-center">
+            <div className="flex flex-wrap md:flex-nowrap gap-4 w-full">
                 <div className="flex flex-col relative w-80 grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole overflow-hidden">
-                    <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute -left-8 blur-xl h-[200%] top-0 w-[2rem]" />
+                    <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute -left-8 blur-xl h-[400%] md:h-[200%] top-0 w-[2rem]" />
                     <div className="flex items-baseline gap-2">
                         <h3>Website Design</h3>
                         <p className="text-start text-sm font-medium bg-clip-text text-transparent bg-gradient-to-r from-indigo-200 to-indigo-500">2-4 days</p>
@@ -26,7 +26,8 @@ export default function ServicesList() {
                         {/* <Image src={arrow} alt="" /> */}
                     </div>
                 </div>
-                <div className="flex flex-col w-80 overflow-hidden grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
+                <div className="flex flex-col relative w-80 overflow-hidden grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
+                <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute -left-8 -top-64 blur-xl h-[400%] w-[2rem] md:hidden" />
                     <div className="flex items-baseline gap-2">
                         <h3>Branding design</h3>
                         <p className="text-start text-sm font-medium bg-clip-text text-transparent bg-gradient-to-r from-indigo-200 to-indigo-500">4-7 days</p>
@@ -44,9 +45,9 @@ export default function ServicesList() {
                 </div>
             </div>
 
-            <div className="flex flex-wrap gap-4 w-full">
+            <div className="flex flex-wrap md:flex-nowrap gap-4 w-full">
                 <div className="flex flex-col w-80 relative overflow-hidden grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
-                <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute lg:hidden -left-8 blur-xl h-[200%] bottom-0 w-[2rem]" />
+                <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute -left-8 blur-xl h-[400%] md:h-[200%] -bottom-64 md:bottom-0 w-[2rem]" />
                     <div className="flex items-baseline gap-2">
                         <h3>Dapp design</h3>
                         <p className="text-start text-sm font-medium bg-clip-text text-transparent bg-gradient-to-r from-indigo-200 to-indigo-500">4-12 days</p>
@@ -62,7 +63,8 @@ export default function ServicesList() {
                         {/* <Image src={arrow} alt="" /> */}
                     </div>
                 </div>
-                <div className="flex flex-col w-80 overflow-hidden grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
+                <div className="flex flex-col w-80 relative overflow-hidden grow gap-6 px-8 py-6 rounded-3xl bg-background border border-transparentbg shadow-innerwhole">
+                <Image priority quality={50} src={gradient} alt="" width={1080} height={1080} className="absolute -left-8 blur-xl h-[400%] bottom-0 w-[2rem] md:hidden" />
                     <div className="flex items-baseline gap-2">
                         <h3>Pitch Deck</h3>
                         <p className="text-start text-sm font-medium bg-clip-text text-transparent bg-gradient-to-r from-indigo-200 to-indigo-500">1-3 days</p>
